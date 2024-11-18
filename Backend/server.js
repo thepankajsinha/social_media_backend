8f783ea9a19e6747da2960125c4809b635d71085
thepankajsinha <askpankajsinha@gmail.com>
2024-11-18 19:03:55 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,10 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json({limit:"5mb"})); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true, // cookies ko frontend me bhejne ke liye
+}));
 
 
 // Routes
